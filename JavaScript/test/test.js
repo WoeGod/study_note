bb5e8588b6ec52aee17a288612361f5caf5b859f
woegod <wuweijia1@gmail.com>
2020-09-19 10:44:55 +0800

    新增 vue源码课程资料

--- JavaScript/test/test.js
+++ JavaScript/test/test.js
@@ -1,35 +1,12 @@
-
-// 缺点：写法繁琐，代码可读性不强
-// for (let i = 0; i < arr.length; i++){
-//   console.log(arr[i]);
-// }
-
-// 优点：简化语法
-// 缺点：不能终止或者跳过
-// lists.forEach(list => {
-//   // if(list === 1){
-//   //   continue  // 报错：SyntaxError: Illegal continue statement: no surrounding iteration statement
-//                  // 使用 break 则报错：Illegal break statement
-//   // }
-//   console.log(list);
-// })
-
-obj = {
-  name : "wu",
-  age: 18,
-  address: 'beijing'
-}
-lists = ['apple','banana','pear','watermelon','pineapple']
-
-for (let index in lists){
-  if(index ==='1'){
-    continue;
+function getByteLen(val) {    //传入一个字符串
+  var len = 0;
+  for (var i = 0; i < val.length; i++) {
+      if (val[i].match(/[^\x00-\xff]/ig) != null) //全角 
+          len += 2; //如果是全角，占用两个字节  如果mysql中某字段是text, 如果设置编码为utf-8,那么一个中文是占3个字节, gbk是两个字节
+      else
+          len += 1; //半角占用一个字节
   }
-  console.log(index);  // 0,1,2,3,4   
-}
+  return len;
+}   
 
-// lists = ['apple','banana','pear','watermelon','pineapple']
-
-// for (let list of lists){
-//   console.log(list);  // apple banana pear watermelon pineapple
-// }
+console.log(getByteLen('shtëpia')); 
---
+++ JavaScript/珍稀接口返回.js
@@ -0,0 +1,89 @@
+{
+  "status": true, 
+  "msg": "成功", 
+  "msg_three": "今日3*3训练成绩,分别获得怪兽数量,训练次数", 
+  "list1": [
+      {
+          "0": {
+              "total": 1, 
+              "per_page": 15, 
+              "current_page": 1, 
+              "last_page": 1, 
+              "data": [
+                  {
+                      "game_time": "1.000", 
+                      "create_time": 1
+                  }
+              ]
+          }, 
+          "1": {
+              "total": 0, 
+              "per_page": 15, 
+              "current_page": 1, 
+              "last_page": 0, 
+              "data": [ ]
+          }, 
+          "count_train": 1
+      }
+  ], 
+  "msg_four": "今日4*4训练成绩,分别获得怪兽数量,训练次数", 
+  "list2": [
+      {
+          "0": {
+              "total": 0, 
+              "per_page": 15, 
+              "current_page": 1, 
+              "last_page": 0, 
+              "data": [ ]
+          }, 
+          "1": {
+              "total": 0, 
+              "per_page": 15, 
+              "current_page": 1, 
+              "last_page": 0, 
+              "data": [ ]
+          }, 
+          "count_train": 0
+      }
+  ], 
+  "msg_five": "今日5*5训练成绩,分别获得怪兽数量,训练次数", 
+  "list3": [
+      {
+          "0": {
+              "total": 0, 
+              "per_page": 15, 
+              "current_page": 1, 
+              "last_page": 0, 
+              "data": [ ]
+          }, 
+          "1": {
+              "total": 0, 
+              "per_page": 15, 
+              "current_page": 1, 
+              "last_page": 0, 
+              "data": [ ]
+          }, 
+          "count_train": 0
+      }
+  ], 
+  "msg_six": "今日6*6训练成绩,分别获得怪兽数量,训练次数", 
+  "list4": [
+      {
+          "0": {
+              "total": 0, 
+              "per_page": 15, 
+              "current_page": 1, 
+              "last_page": 0, 
+              "data": [ ]
+          }, 
+          "1": {
+              "total": 0, 
+              "per_page": 15, 
+              "current_page": 1, 
+              "last_page": 0, 
+              "data": [ ]
+          }, 
+          "count_train": 0
+      }
+  ]
+}
