d8c1405902207cf5981f8127fcf787da2b4f34eb
Wu <wuweijia1@gmail.com>
2021-01-08 14:18:01 +0800

    面试笔记补充

--- Interview/test/test.js
+++ Interview/test/test.js
@@ -1,4 +1,16 @@
-let arr = [{a:1}]
-let arr2 = Array.from(arr)
-arr2[0].a = 2;
+let arr = [2,1,0,-1,3,6,4]
+
+for(let i = 0; i < arr.length - 1 ; i++){
+  let flag = false
+  for(let j = 0; j < arr.length - 1; j++){
+    if(arr[j] > arr[j+1]){
+      [arr[j],arr[j+1]] = [arr[j+1],arr[j]]
+      flag = true
+    }
+  }
+  if(flag){
+    break
+  }
+}
+
 console.log(arr);
